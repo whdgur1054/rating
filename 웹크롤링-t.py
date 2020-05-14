1cc2d2ad461c9cd3a4ca56ee15576e91691d23a2
whdgur1054 <50695077+whdgur1054@users.noreply.github.com>
2020-05-14 13:55:35 +0900

    Add files via upload

---
+++ 웹크롤링-t.py
@@ -0,0 +1,208 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import pymysql
+import datetime
+import time
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+
+conn = pymysql.connect(host = 'localhost', user = 'test', password = 'testing',
+                       db = 'test', charset='utf8')
+curs = conn.cursor()
+#sql = "select * from game_info"
+#curs.execute(sql)
+
+#rows = curs.fetchall()
+#print(rows)
+
+print("\n\n")
+
+#url = "https://store.steampowered.com/app/397540"
+#driver = webdriver.Chrome()
+driver = webdriver.Chrome(ChromeDriverManager().install())
+#driver.implicitly_wait(5)
+
+
+f_r = open("applist_1.txt",'r')
+lines = f_r.readlines()
+
+
+def setting():
+        
+    url = "https://store.steampowered.com/app/" + "550"
+    driver.get(url)
+    bsObject = BeautifulSoup(driver.page_source, 'html.parser')
+
+    dayField = driver.find_element_by_id("ageDay")
+    dayField.send_keys("11")
+    monthField = driver.find_element_by_id("ageMonth")
+    monthField.send_keys("Octorber")
+    yearField = driver.find_element_by_id("ageYear")
+    yearField.send_keys("1995", Keys.TAB, Keys.ENTER)
+
+
+    url = "https://store.steampowered.com/app/" + "550"
+    driver.get(url)
+    bsObject = BeautifulSoup(driver.page_source, 'html.parser')
+    language = driver.find_element_by_id("language_pulldown")
+    language.click()
+    driver.find_element_by_xpath('//*[@onclick="ChangeLanguage( \'koreana\' ); return false;"]').click()
+    #driver.implicitly_wait(15)
+    time.sleep(1)
+
+setting()
+
+for i in range(35000,36000):
+    url = "https://store.steampowered.com/app/" + lines[i]
+    driver.get(url)
+    bsObject = BeautifulSoup(driver.page_source, 'html.parser')
+    print("\t*****%d*****" %i)
+
+    data = "\n**%d" %i
+    f = open("ERROR.txt", 'a',-1,"utf-8")
+    f.write(data)
+    f.close()
+    
+    try:                                                                                                    #Get_Title
+        title = bsObject.find('div',{'class' : "apphub_AppName"}).get_text()
+        dev_row = bsObject.find_all('div',{'class' : "dev_row"})
+    except Exception as e:
+        title_name = bsObject.head.title.get_text()
+        if(title_name != "Steam에 오신 것을 환영합니다"):
+            print ("\t**** ERROR PAGE ****")
+            data = "Error : %d line...  Error Page : %s" %(i,e)
+            f = open("ERROR.txt", 'a',-1,"utf-8")
+            f.write(data)
+            f.close()
+        continue
+    
+    try:                                                                                                    #Get_Price
+        price = int (bsObject.find('div',{'class' : "game_area_purchase_game_wrapper"}).find('div',{'class' : "discount_original_price"}).get_text().replace(",","")[2:])
+    except Exception as e:  #game_purchase_price price
+        try:
+            price = int (bsObject.find('div',{'class' : "game_area_purchase_game_wrapper"}).find('div',{'class' : "game_purchase_price price"}).get_text().strip().replace(",","")[2:])
+        except:
+            price = 0
+
+    print(title)
+    print(price)
+    #print(dev_row)
+
+                                                                                                            #TypeChecking
+    type = bsObject.find('div' ,{'class' : "blockbg"}).get_text()
+    #print(type)
+    if(type.find("모든 게임")>0):
+        if(type.find("다운로드 가능한 콘텐츠")>0):
+            print("DLC")
+            type = "DLC"
+        else:
+            print("GAME")
+            type = "GAME"
+    elif(type.find("모든 소프트웨어")>0):
+        print("Software")
+        continue
+    elif(type.find("모든 사운드트랙")>0):
+        print("soundtrack")
+        continue
+        
+    try:                                                                                                    #Get_Publisher and Developer
+        dev = dev_row[2].text.rstrip('\n')
+        temp = dev.find(":")
+        dev = dev[temp+1:].strip()
+    except:
+        dev = ""
+    print(dev)
+    
+    try:
+        pub = dev_row[3].text
+        temp = pub.find(":")
+        pub = pub[temp+1:].strip()
+    except:
+        pub=""
+    print(pub)
+    
+    try:                                                                                                    #Get_ReleaseDate
+        release = bsObject.find('div',{'class' : 'date'}).get_text()
+        release = datetime.datetime.strptime(release, '%Y년 %m월 %d일').date()
+    except:
+        try:
+            release = datetime.datetime.strptime(release, '%b, %Y').date()
+        except:
+            release = 00000000
+    print("%s" %release)
+
+
+    try:                                                                                                    #Get_Description
+        desc = bsObject.find('div', {'class' : "game_description_snippet"}).get_text().strip()
+    except:
+        desc = ""
+    print(desc)
+
+    try:
+        genre_all = bsObject.find('div',{'class' :"glance_tags popular_tags"}).get_text().replace("\t","")
+        genre_all = genre_all[1:].lstrip("\n").replace("\n","|").replace("+","")
+        #print(genre_all)
+    except:
+        genre_1=""
+        genre_2=""
+        genre_3=""
+
+    try:                                                                                                    #Get_Genre
+        temp = genre_all.find("|")
+        genre_1 = genre_all[:temp].strip()
+        genre_all = genre_all[temp+1:]
+        print("%s" %genre_1)
+    except:
+        genre_1=""
+        genre_2=""
+        genre_3=""
+        
+    try:
+        temp = genre_all.find("|")
+        genre_2 = genre_all[:temp].strip()
+        genre_all = genre_all[temp+1:]
+        print("%s" %genre_2)
+    except:
+        genre_2=""
+        genre_3=""
+        
+    try:
+        temp = genre_all.find("|")
+        genre_3 = genre_all[:temp].strip()
+        genre_all = genre_all[temp+1:]
+        print("%s" %genre_3)
+    except:
+        genre_3=""
+
+    try:                                                                                                    #Get_GameRate
+        point_all = bsObject.find('div',{'itemprop' : "aggregateRating"}).get_text()
+        temp = point_all.find('%')
+        point = point_all[temp-2:temp]
+        temp_start = point_all.find('the')
+        temp_end = point_all.find('user')
+        user_num = int (point_all[temp_start+4 : temp_end].strip().replace(",",""))
+    except:
+        user_num = 0
+        point = 0
+    print("%s" %point)
+    print("%d" %user_num)
+
+    
+    try:                                                                                                    #Set_MySQL
+        sql = "insert into game_info value (%s, %s, %s, %s, %s, %s, %s, %s, %s)" 
+        curs.execute(sql, (title, type, price, dev, pub, release, point, user_num, desc))
+        sql = "insert into game_genre value (%s, %s, %s, %s)"
+        curs.execute(sql, (title, genre_1, genre_2, genre_3))
+        conn.commit()
+    except Exception as e:
+        print(e)
+        data = "Error %d line ...  Sql error : %s" %(i, e)
+        f = open("ERROR.txt", 'a',-1,"utf-8")
+        f.write(data)
+        f.close()
+        continue
+    print("-----------------------------------------\n\n")
+
+conn.close()
